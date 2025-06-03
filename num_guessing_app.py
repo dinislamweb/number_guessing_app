c17433343a76b79581fe956bf3937c815e225f81
dinislamweb <96992849+dinislamweb@users.noreply.github.com>
2025-06-03 11:38:46 +0600

    added first code

---
+++ num_guessing_app.py
@@ -0,0 +1,15 @@
+from random import randint
+
+random_num = randint(1, 100)
+c = 0  
+
+while True:
+    guess = int(input("Enter your guess number from 1-100: "))
+    c += 1
+    if guess < random_num:
+        print("Too Low!")
+    elif guess > random_num:
+        print("Too High!")
+    else:
+        print(f"You guessed it right at {c} attempts!")
+        break
